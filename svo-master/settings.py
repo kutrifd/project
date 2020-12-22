179b0ecdfa452ea96b037f3f84cc7aa9ca8e0c7e
kutrifd <76441771+kutrifd@users.noreply.github.com>
2020-12-22 11:04:40 +0900

    Update settings.py

--- svo-master/settings.py
+++ svo-master/settings.py
@@ -1,5 +1,22 @@
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
+
+
 import os
 from os import environ
+
+
 
 #import dj_database_url
 
@@ -99,3 +116,8 @@ SESSION_CONFIGS = [
 # anything you put after the below line will override
 # oTree's default settings. Use with caution.
 otree.settings.augment_settings(globals())
+
+
+
+
+
